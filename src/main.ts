4fa1900b30080ed18745d8d432bea1e0b7c6481c
Ravuthz <ravuthz@gmail.com>
2023-09-26 22:38:35 +0700

    Add dotenv and config with example

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT || 3000;
+const logger = new Logger('NestApplication');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();

5c3d4cd6002cafae946de72e2f93ee8f10705eec
Ravuthz <ravuthz@gmail.com>
2023-09-26 22:42:03 +0700

    Add class-transformer, class-validator and enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 const port = process.env.PORT || 3000;
 const logger = new Logger('NestApplication');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({ transform: true, enableDebugMessages: false }),
+  );
+
   await app.listen(port);
   logger.log(`Application is running on: ${await app.getUrl()}`);
 }
